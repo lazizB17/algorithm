b65b9421b073b935fed1a651e3c81d39752dd0b1
lazizB17 <abdulazizboriyev993@gmail.com>
2022-04-05 09:40:30 +0500

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,18 @@
+
+import 'dart:io';
+
+void main(){
+
+  stdout.write("Enter number 1: ");
+  num son1 = num.parse(stdin.readLineSync()!);
+  stdout.write("Enter number 2: ");
+  num son2 = num.parse(stdin.readLineSync()!);
+  print(addRight(son1, son2));
+}
+
+addRight(num a, num r){
+  String c = a.toString();
+  String s = r.toString();
+  num add = num.parse(c + s);
+  return add;
+}
---
+++ test/algoritm1_m_test.dart
@@ -0,0 +1,8 @@
+import 'package:algoritm1_m/algoritm1_m.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
